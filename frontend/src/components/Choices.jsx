97b2d388c496a926db76e58eec89905750204470
Anshu Chaudhary <146634082+anshuchaudharyiitbhu@users.noreply.github.com>
2025-07-20 23:27:34 +0530

    Update Choices.jsx

--- frontend/src/components/Choices.jsx
+++ frontend/src/components/Choices.jsx
@@ -1,27 +1,20 @@
-import React, { useContext } from 'react'
-import Choice_box from './Choice_box'
-import { JobContext } from './context/Jobcontext.jsx'
-import { SearchedContext } from './context/searchedcontext'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import Choice_box from './Choice_box';
 
 const Choices = () => {
-    const {searched,setsearched}=useContext(SearchedContext);
-    const navigate=useNavigate();
-    return (
-        <div>
-            <div className=" w-[70vw] choices grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 justify-center items-center mx-auto">
-                
-                <Choice_box onClick={(e)=>{navigate('/browse'),setsearched(e.target.choice)}} choice="Remote" link="https://static.naukimg.com/s/0/0/i/trending-naukri/remoteonetheme.svg"/>
-                <Choice_box onClick={(e)=>{navigate('/browse'),setsearched(e.target.choice)}} choice="MNC" link="https://static.naukimg.com/s/0/0/i/trending-naukri/mnconetheme.svg"/>
-                <Choice_box onClick={(e)=>{navigate('/browse'),setsearched(e.target.choice)}} choice="Banking & Finance" link="https://static.naukimg.com/s/0/0/i/trending-naukri/financeonetheme.svg"/>
-                <Choice_box onClick={(e)=>{navigate('/browse'),setsearched(e.target.choice)}} choice="Analytics" link="https://static.naukimg.com/s/0/0/i/trending-naukri/analyticsonetheme.svg"/>
-                <Choice_box onClick={(e)=>{navigate('/browse'),setsearched(e.target.choice)}} choice="Data Science" link="https://static.naukimg.com/s/0/0/i/trending-naukri/data-scienceonetheme.svg" />
-                <Choice_box onClick={(e)=>{navigate('/browse'),setsearched(e.target.choice)}} choice="Software & IT" link="https://static.naukimg.com/s/0/0/i/trending-naukri/softwareonetheme.svg" />
-                <Choice_box onClick={(e)=>{navigate('/browse'),setsearched(e.target.choice)}} choice="Business" link="https://static.naukimg.com/s/0/0/i/trending-naukri/startuponetheme.svg"/>
-            </div>
+  return (
+    <div className="flex justify-center">
+      <div className="w-[90vw] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        <Choice_box choice="Remote" link="https://static.naukimg.com/s/0/0/i/trending-naukri/remoteonetheme.svg" />
+        <Choice_box choice="MNC" link="https://static.naukimg.com/s/0/0/i/trending-naukri/mnconetheme.svg" />
+        <Choice_box choice="Banking & Finance" link="https://static.naukimg.com/s/0/0/i/trending-naukri/financeonetheme.svg" />
+        <Choice_box choice="Analytics" link="https://static.naukimg.com/s/0/0/i/trending-naukri/analyticsonetheme.svg" />
+        <Choice_box choice="Data Science" link="https://static.naukimg.com/s/0/0/i/trending-naukri/data-scienceonetheme.svg" />
+        <Choice_box choice="Software & IT" link="https://static.naukimg.com/s/0/0/i/trending-naukri/softwareonetheme.svg" />
+        <Choice_box choice="Business" link="https://static.naukimg.com/s/0/0/i/trending-naukri/startuponetheme.svg" />
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Choices
+export default Choices;
