1a924e49814bb991c4face514391293de1f9317f
Anshu Chaudhary <146634082+anshuchaudharyiitbhu@users.noreply.github.com>
2026-07-23 17:58:55 +0530

    Update constant.js

--- frontend/src/utilis/constant.js
+++ frontend/src/utilis/constant.js
@@ -1,1 +1,2 @@
 export const USER_API_END_POINT ="https://pathforward-job-portal-backend-oc5e.onrender.com/api/v1/user";
+export const API_END_POINT ="https://pathforward-job-portal-backend-oc5e.onrender.com/api/v1";
