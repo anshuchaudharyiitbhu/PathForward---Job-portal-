f1ee5ee4de3ca74d54b8d5debcc392f4a5a768e6
Anshu Chaudhary <146634082+anshuchaudharyiitbhu@users.noreply.github.com>
2026-07-23 17:59:54 +0530

    Update Adminjobcontext.jsx

--- frontend/src/components/context/Adminjobcontext.jsx
+++ frontend/src/components/context/Adminjobcontext.jsx
@@ -7,7 +7,7 @@ export const AdminJobProvider = ({ children }) => {
   const [ajob, setajob] = useState(null);
   const fetchJobs = async () => {
       try {
-        const response = await axios.get("https://pathforward-job-portal-backend.onrender.com/api/v1/job/getadminjobs",{withCredentials:true});
+        const response = await axios.get(`${API_END_POINT}/job/getadminjobs`,{withCredentials:true});
         setajob(response.data.jobs || []);
         // console.log(job);
         
